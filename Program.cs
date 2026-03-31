992a1c2ea64fa9a4a4e9f326a21714cd87f94d80
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 07:39:51 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class PemrosesData
+{
+    public T DapatkanNilaiTerbesar<T> (T nilai1, T nilai2, T nilai3)
+    {
+        dynamic nil1 = (dynamic)nilai1;
+        dynamic nil2 = (dynamic)nilai2;
+        dynamic nil3 = (dynamic)nilai3;
+        dynamic max = 0;
+
+        if (nil2 > max)
+        {
+            max = nil2;
+        }
+
+        if (nil3 > max)
+        {
+            max = nil3;
+        }
+
+        return max;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PemrosesData nilai = new PemrosesData();
+        int max = nilai.DapatkanNilaiTerbesar(10, 30, 22);
+
+        Console.WriteLine($"Nilai Terbesar : {max}");
+    }
+}
--- obj/Debug/net10.0/Modul5_103022400026.AssemblyInfo.cs
+++ obj/Debug/net10.0/Modul5_103022400026.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Modul5_103022400026")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+984d95bc6b3d882313fb30aced24450a867f3e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("Modul5_103022400026")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Modul5_103022400026")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
