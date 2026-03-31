da1fffeb66ee6145445bd73aa1ce17f639758757
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2026-03-31 08:11:11 +0700

    commit message

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i + 1) + " berisi : " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<float> data = new SimpleDataBase<float>();
+        data.AddNewData(10);
+        data.AddNewData(30);
+        data.AddNewData(22);
+
+        data.PrintAllData();
+    }
+}
